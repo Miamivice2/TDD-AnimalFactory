adda2bc5625dfd8361078b510be901c3330fc98e
Miamivice2 <andreb.davis2@gmail.com>
2023-04-19 11:26:18 -0400

    finished last test

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,5 +1,13 @@
 package rocks.zipcodewilmington;
 
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
@@ -9,4 +17,19 @@ public class CatHouseTest {
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfCats()`
+
+
+    public void testGetNumberOfCats() {
+        // Given (some
+        String name = "Milo";
+        Date birthDate = new Date();
+        Cat animal = AnimalFactory.createCat(name, birthDate);
+        CatHouse.clear();
+
+        // When
+        CatHouse.add(animal);
+
+        // Then
+        CatHouse.getNumberOfCats();
+    }
 }
